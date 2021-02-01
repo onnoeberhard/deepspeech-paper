dda2b90e5a520a5f19adbef002c044ff3a21b929
Onno Eberhard <onnoeberhard@gmail.com>
2021-02-01 13:54:57 +0100

    cleanup

--- training.sh
+++ training.sh
@@ -1,3 +1,4 @@
+# --- FIRST PHASE (German, complete training) ---
 # Working directory
 mkdir deepspeech
 cd deepspeech
@@ -73,7 +74,8 @@ nohup python DeepSpeech/DeepSpeech.py --train_files common_voice/clips/train.csv
 # View output of training process
 tail -f training.log
 
-# --- SECOND PHASE --- (still inside deepspeech working dir with deepspeech conda env activated)
+# --- SECOND PHASE (German, Simple Transfer) ---
+# (still inside deepspeech working dir with deepspeech conda env activated)
 # Set up working directory
 mkdir ../simple-transfer
 mv -t ../simple-transfer common_voice kenlm.scorer DeepSpeech
@@ -125,7 +127,8 @@ nohup DeepSpeech/DeepSpeech.py \
 # View output of testing process
 tail -f training.log
 
-# --- THRID PHASE --- (still inside simple-transfer working dir with deepspeech conda env activated)
+# --- THRID PHASE (German, three frozen layers) ---
+# (still inside simple-transfer working dir with deepspeech conda env activated)
 # Set up working directory
 mkdir ../finetune
 mv -t ../finetune common_voice kenlm.scorer alphabet.txt pretrained
@@ -186,7 +189,8 @@ nohup deepspeech-transfer/DeepSpeech.py \
 # View output of testing process
 tail -f training.log
 
-# --- FOURTH PHASE --- (still inside finetune working dir with ds-finetune conda env activated)
+# --- FOURTH PHASE (German, 1 frozen layer) ---
+# (still inside finetune working dir with ds-finetune conda env activated)
 # Set up working directory
 mkdir ../finetune-1layer
 mv -t ../finetune-1layer common_voice kenlm.scorer alphabet.txt pretrained
@@ -239,7 +243,8 @@ nohup deepspeech-transfer/DeepSpeech.py \
 # View output of testing process
 tail -f testing.log
 
-# --- FIFTH PHASE --- (still inside finetune-1layer working dir with ds-finetune conda env activated)
+# --- FIFTH PHASE (German, 2 frozen layers) ---
+# (still inside finetune-1layer working dir with ds-finetune conda env activated)
 # Set up working directory
 mkdir ../finetune-2layer
 mv -t ../finetune-2layer common_voice kenlm.scorer alphabet.txt pretrained
@@ -292,7 +297,8 @@ nohup deepspeech-transfer/DeepSpeech.py \
 # View output of testing process
 tail -f testing.log
 
-# --- SIXTH PHASE --- (still inside finetune-2layer working dir with ds-finetune conda env activated)
+# --- SIXTH PHASE (German, 4 frozen layers) ---
+# (still inside finetune-2layer working dir with ds-finetune conda env activated)
 # Set up working directory
 mkdir ../finetune-4layer
 mv -t ../finetune-4layer common_voice kenlm.scorer alphabet.txt pretrained
@@ -344,3 +350,343 @@ nohup deepspeech-transfer/DeepSpeech.py \
 
 # View output of testing process
 tail -f testing.log
+
+# --- SEVENTH PHASE (Swiss German, complete training) ---
+# (still inside finetune-4layer working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../swiss/complete
+mv -t ../swiss/complete pretrained alphabet.txt kenlm.scorer
+cd ../swiss/complete
+
+# Edit alphabet: add numbers 0-9
+nano alphabet.txt
+
+# Download and extract Swiss German speech data
+pip install gdown
+gdown https://drive.google.com/uc?id=1ig4KqzwVnZgiI513Z9s2SAa9BPwPPwjT
+tar -xvf zhaw-splits.tar.gz
+mv zhaw-splits swiss_data
+rm zhaw-splits.tar.gz
+mv swiss_data/*.csv swiss_data/clips
+
+# Clone and install DeepSpeech
+git clone https://github.com/mozilla/DeepSpeech.git
+cd DeepSpeech
+git reset --hard v0.7.3
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup DeepSpeech/DeepSpeech.py \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup DeepSpeech/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- EIGHTH PHASE (Swiss German, Simple Transfer) ---
+# (still inside swiss/complete working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../simple-transfer
+mv -t ../simple-transfer pretrained alphabet.txt kenlm.scorer DeepSpeech swiss_data
+cd ../simple-transfer
+
+# Clone and install DeepSpeech
+cd DeepSpeech
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup DeepSpeech/DeepSpeech.py \
+    --drop_source_layers 1 \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --load_checkpoint_dir pretrained \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup DeepSpeech/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- NINTH PHASE (Swiss German, 1 frozen layer) ---
+# (still inside swiss/simple-transger working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../freeze1
+mv -t ../freeze1 pretrained alphabet.txt kenlm.scorer swiss_data
+cd ../freeze1
+
+# Clone and install deepspeech-transfer and install pip dependencies
+git clone https://github.com/onnoeberhard/deepspeech-transfer
+cd deepspeech-transfer
+git checkout transfer-1
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup deepspeech-transfer/DeepSpeech.py \
+    --drop_source_layers 1 \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --load_checkpoint_dir pretrained \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup deepspeech-transfer/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- TENTH PHASE (Swiss German, 2 frozen layers) ---
+# (still inside swiss/freeze1 working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../freeze2
+mv -t ../freeze2 pretrained alphabet.txt kenlm.scorer swiss_data deepspeech-transfer
+cd ../freeze2
+
+# Install deepspeech-transfer
+cd deepspeech-transfer
+git checkout transfer-2
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup deepspeech-transfer/DeepSpeech.py \
+    --drop_source_layers 1 \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --load_checkpoint_dir pretrained \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup deepspeech-transfer/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- ELEVENTH PHASE (Swiss German, 3 frozen layers) ---
+# (still inside swiss/freeze2 working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../freeze3
+mv -t ../freeze3 pretrained alphabet.txt kenlm.scorer swiss_data deepspeech-transfer
+cd ../freeze3
+
+# Install deepspeech-transfer
+cd deepspeech-transfer
+git checkout transfer
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup deepspeech-transfer/DeepSpeech.py \
+    --drop_source_layers 1 \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --load_checkpoint_dir pretrained \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup deepspeech-transfer/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- TWELFTH PHASE (Swiss German, 4 frozen layers) ---
+# (still inside swiss/freeze3 working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../freeze4
+mv -t ../freeze4 pretrained alphabet.txt kenlm.scorer swiss_data deepspeech-transfer
+cd ../freeze4
+
+# Install deepspeech-transfer
+cd deepspeech-transfer
+git checkout transfer-4
+pip install -e .
+cd ..
+
+# Remove files from previous trainings
+rm -rf ~/.local/share/deepspeech/summaries
+rm -rf ~/.local/share/deepspeech/checkpoints
+
+# Train model
+nohup deepspeech-transfer/DeepSpeech.py \
+    --drop_source_layers 1 \
+    --train_files swiss_data/clips/train.csv \
+    --dev_files swiss_data/clips/dev.csv \
+    --test_files swiss_data/clips/test.csv \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint \
+    --load_checkpoint_dir pretrained \
+    --scorer kenlm.scorer \
+    --test_batch_size 36 \
+    --train_batch_size 24 \
+    --dev_batch_size 36 \
+    --epochs 30 \
+    --learning_rate 0.0005 \
+    --dropout_rate 0.40 \
+    --train_cudnn \
+    --export_dir models \
+    </dev/null >training.log 2>&1 &
+
+# View output of training process
+tail -f training.log
+
+# Testing
+nohup deepspeech-transfer/DeepSpeech.py \
+    --alphabet_config_path alphabet.txt \
+    --save_checkpoint_dir savecheckpoint_test \
+    --load_checkpoint_dir savecheckpoint \
+    --test_files swiss_data/clips/test.csv \
+    --test_batch_size 36 \
+    --scorer kenlm.scorer \
+    </dev/null >testing.log 2>&1 &
+
+# View output of testing process
+tail -f testing.log
+
+# --- THIRTEENTH PHASE (Swiss German from German checkpoint, complete training) ---
+# (still inside swiss/freeze4 working dir with ds-finetune conda env activated)
+# Set up working directory
+mkdir -p ../../swiss_de/complete
+mv -t ../../swiss_de/complete alphabet.txt kenlm.scorer swiss_data deepspeech-transfer
+cd ../../swiss_de/complete
+mv 
+
+
+# curl --upload-file training.log https://filepush.co/upload/training-4l.log
+# curl --upload-file testing.log https://filepush.co/upload/testing-4l.log
